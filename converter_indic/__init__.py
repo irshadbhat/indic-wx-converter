faa9253582106997d5968fc4a44ef7414d735ea6
Irshad Bhat <bhatirshad127@gmail.com>
2015-09-30 12:36:00 +0530

    added support for ssf

--- converter_indic/__init__.py
+++ converter_indic/__init__.py
@@ -24,6 +24,7 @@ def main():
     #trg_enc_help = "select output-file encoding [utf|wx]"
     format_help  = "select output format [text|ssf|conll|bio|tnt]"
     lang_help    = "select language [%s] (3 letter ISO-639 code)" %('|'.join(languages))
+    ssf_help     = "Specify ssf-type [inter|intra]"
 
     # parse command line arguments 
     parser = argparse.ArgumentParser(prog="converter-indic", description="wx-utf converter for Indain languages")
@@ -32,10 +33,15 @@ def main():
     parser.add_argument('--s', metavar='source', dest="src_enc", choices=["utf","wx"], default="utf", help="%s" %src_enc_help)
     #parser.add_argument('--t', metavar='target', dest="trg_enc", choices=["utf","wx"], default="wx", help="%s" %trg_enc_help)
     parser.add_argument('--f', metavar='format', dest="format_", choices=format_list, default="text", help="%s" %format_help)
+    parser.add_argument('--t', metavar='ssf-type', dest="ssf_type", choices=["inter","intra"], default=None, help=ssf_help)
     parser.add_argument('--i', metavar='input', dest="INFILE", type=argparse.FileType('r'), default=sys.stdin, help="<input-file>")
     parser.add_argument('--o', metavar='output', dest="OUTFILE", type=argparse.FileType('w'), default=sys.stdout, help="<output-file>")
 
     args = parser.parse_args()
+    if args.format_ == 'ssf' and not args.ssf_type:
+	sys.stderr.write(parser.format_usage())
+	sys.stderr.write("converter-indic: error: argument --t: not specified\n")
+	sys.exit(0)
 
     # set conversion direction
     if args.src_enc=="utf": #and args.trg_enc=="wx":
@@ -44,7 +50,7 @@ def main():
         src_trg = "wx2utf"
 
     # initialize converter object
-    con = wxConvert(src_trg, args.format_, args.lang)
+    con = wxConvert(src_trg, args.format_, args.lang, args.ssf_type)
     # convert text
     if args.format_ == "ssf":
 	sentences = re.finditer("(<Sentence id=.*?>)(.*?)</Sentence>", args.INFILE.read(), re.S)
