cdf12955788967047974d0c278250389bbb8a95d
Irshad Ahmad Bhat <bhatirshad127@gmail.com>
2016-01-25 16:20:16 +0530

    fixed a bug while masking iscii characters

--- converter_indic/wxILP.py
+++ converter_indic/wxILP.py
@@ -34,7 +34,7 @@ class wxilp():
         self.iscii_num = dict(zip([unichr(i) for i in range(161, 252)], ['\x03%s\x04' %(unichr(i)) for i in range(300, 391)]))
         self.num_iscii = dict(zip(['\x03%s\x04' %(unichr(i)) for i in range(300, 391)], [unichr(i) for i in range(161, 252)]))
         self.mask_isc = re.compile(u'([\xA1-\xFB])') 
-        self.unmask_isc = re.compile(u'(%s)' %'|'.join(['\x03%s\x04' %(unichr(i)) for i in range(300, 390)]))
+        self.unmask_isc = re.compile(u'(%s)' %'|'.join(['\x03%s\x04' %(unichr(i)) for i in range(300, 391)]))
 
         if self.order == "utf2wx":
             if self.lang_tag == 'urd':
