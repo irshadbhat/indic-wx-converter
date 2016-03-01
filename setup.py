d4fd9ffbdaa01d43a453764fb39300316f6f6c2b
Irshad Ahmad Bhat <bhatirshad127@gmail.com>
2016-03-01 18:43:02 +0530

    update

--- setup.py
+++ setup.py
@@ -92,7 +92,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
         "Operating System :: OS Independent"
     ],
 
