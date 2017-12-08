44bb452f3d05681d7235676b30d5c314359e7994
irshadbhat <bhatirshad127@gmail.com>
2017-12-08 17:27:43 +0530

    allow installation from zip file

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup
+
+os.environ['PBR_VERSION'] = '1.2.3'
+os.environ['SKIP_WRITE_GIT_CHANGELOG'] = '1'
+os.environ['SKIP_GENERATE_AUTHORS'] = '1'
 
 setup(
     setup_requires=['pbr'],
