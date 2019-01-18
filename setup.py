d41b8f725524791ef032487a4e9782818e56c5d5
irshadbhat <bhatirshad127@gmail.com>
2019-01-18 22:14:54 +0530

    pbr version update

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 import os
 from setuptools import setup
 
-os.environ['PBR_VERSION'] = '1.2.3'
+os.environ['PBR_VERSION'] = '2.0.0'
 os.environ['SKIP_WRITE_GIT_CHANGELOG'] = '1'
 os.environ['SKIP_GENERATE_AUTHORS'] = '1'
 
 setup(
-    setup_requires=['pbr'],
+    setup_requires=['pbr>=2.0', 'setuptools>=17.1'],
     pbr=True,
     include_package_data=True
 )
